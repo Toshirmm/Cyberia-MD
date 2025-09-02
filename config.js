295f158ced259051b054f9cbcefa4f1d77aeb31c
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2025-09-02 16:44:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~M3lGWaDS#uSTShpKuLJxLXetdmwZDqo9VGbpVNCOgFbUoZKx51Sc",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "27649342626", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
