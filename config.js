b386e7f19079071b163d698da2ebef6e3cb75dbc
Toshirmm <144406472+Toshirmm@users.noreply.github.com>
2025-12-31 19:10:28 -0300

    Update SESSION_ID and OWNER_NUMBER in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "27649342626", // PUT OWNER NUMBER HERE
+  SESSION_ID: " Cck2mQjC#qWPy8algfzrDNIlW6qZzlOafFpTBcYcdGa7LWrWUGPI",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "56923873781", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
